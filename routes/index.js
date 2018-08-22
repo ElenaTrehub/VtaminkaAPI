c4af2a48663809ac6152f141fb720c1b0e929003
mrvet <alexoovsyanikov@gmail.com>
2018-08-22 12:12:41 +0300

    Dev start

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 const HomeController = require('../controller/panel/HomeController');
 
 const CategoryController = require('../controller/panel/CategoryController');
